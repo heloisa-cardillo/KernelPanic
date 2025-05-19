431764834a3c875fd23665da9d5134ede1352d17
VitorSerpa <vitorserpa123@gmail.com>
2025-05-19 19:14:09 -0300

    feat(Insights):Insight page criada e estilizada

--- src/frontend-API/app.py
+++ src/frontend-API/app.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template, request, jsonify
 from query import montar_query
 from query import montar_query_top5
+from dotenv import load_dotenv
 import pymysql
 import os
+
+load_dotenv()
 
 app = Flask(__name__, static_folder='static')
 
@@ -17,6 +20,7 @@ def get_db_connection():
     )
     print("Conexão feita!")
     return conn
+
 
 @app.route("/")
 def index():
@@ -38,6 +42,11 @@ def chartPage():
 @app.route("/sobre.html")
 def sobre():
     return render_template('sobre.html')
+
+@app.route("/insights")
+def insightsPage():
+
+    return render_template('insights.html')
 
 @app.route('/filtros', methods=['POST'])
 def filtros_dados():
